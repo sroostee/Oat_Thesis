e547018c08161c7fb8bda858c1a2eedfb8845c49
Suze <su2781ro-s@student.lu.se>
2019-12-10 15:02:03 +0100

    Add file to test conv3 layers

---
+++ scripts/3D_analysis/fastai3D/conv3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+#from fastai import *
+#from fastai.vision import *
+#from torch.nn import *
+
+import numpy as np
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+seed = 42
+np.random.seed(seed)
+torch.manual_seed(seed)
+
+#The compose function allows for multiple transforms
+#transforms.ToTensor() converts our PILImage to a tensor of shape (C x H x W) in the range [0,1]
+#transforms.Normalize(mean,std) normalizes a tensor to a (mean, std) for (R, G, B)
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+train_set = torchvision.datasets.CIFAR10(root='./cifardata', train=True, download=False, transform=transform)
+
+test_set = torchvision.datasets.CIFAR10(root='./cifardata', train=False, download=False, transform=transform)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
